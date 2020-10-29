8ac890aed89948280bf4c66f8ede1b70f9e91865
RickardVMiun <rivi2000@student.miun.se>
2020-10-29 10:06:45 +0100

    Lämnar in uppgift 10b

---
+++ Labbar/Uppgift10b/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        int rättSvar;
+        int användarensSvar;
+        int avvikelse;
+        int antalGissningar;
+        
+        
+        
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void slumpatal_Click(object sender, RoutedEventArgs e)
+        {
+            Gissa.IsEnabled = true;
+            rättSvar = random.Next(0, 1000);
+            // resultattext.Text = Convert.ToString(rättSvar);
+
+        }
+
+        private void Gissa_Click(object sender, RoutedEventArgs e)
+        {
+
+                
+                användarensSvar = int.Parse(talet.Text);
+                avvikelse = Math.Abs(rättSvar - användarensSvar);
+
+                if (användarensSvar == rättSvar)
+                {
+                    resultattext.Text = $"Rätt svar! Det tog dig {antalGissningar} försök.";
+                }
+                if (användarensSvar < rättSvar && avvikelse < 100)
+                {
+                    resultattext.Text = "Oj, du gissade lite för högt!";
+                    antalGissningar++;
+                }
+                else if (användarensSvar > rättSvar && avvikelse < 100)
+                {
+                    resultattext.Text = "Oj, du gissade lite för lågt!";
+                    antalGissningar++;
+                }
+                else if (användarensSvar > rättSvar && avvikelse > 100)
+                {
+                    resultattext.Text = "Du gissade alldeles för högt.";
+                    antalGissningar++;
+                }
+                else if (användarensSvar < rättSvar && avvikelse > 100)
+                {
+                    resultattext.Text = "Du gissade alldeles för lågt.";
+                    antalGissningar++;
+                }
+            
+            
+        }
+    }
+}
