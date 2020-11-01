314674b66b62da85f3d8ce2d9ce488f0f7eebf57
RickardVMiun <rivi2000@student.miun.se>
2020-11-01 11:16:00 +0100

    Lämnar in uppgift 13

---
+++ Labbar/Uppgift13/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift13
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string användarensMening = userInput.Text;
+            int count = 0;
+            char konverteradChar = Convert.ToChar(userLetter.Text);
+            foreach (char c in användarensMening)
+            {
+                if (c == konverteradChar)
+                {
+                    count++;
+                }
+            }
+            letterResult.Content = $"Det finns {count} stycken {konverteradChar} i din mening.";
+        }
+    }
+}
